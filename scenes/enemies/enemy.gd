610e327dcb417be60efca76d3f9f92d9c2d12271
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2024-08-16 17:07:37 -0700

    Add some basic enemy logic

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Enemy
 extends Node
 
@@ -8,3 +9,18 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pass
+
+
+func get_area() -> Area2D:
+	var area = get_node_or_null("Area2D")
+	if area is Area2D:
+		return area
+	return null
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	var area = get_area()
+	if !is_instance_valid(area):
+		warnings.push_back("Drop scenes must have a child node named 'Area2D' of type Area2D.")
+	return warnings
---
+++ scripts/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var level: Level
+var player: Player
