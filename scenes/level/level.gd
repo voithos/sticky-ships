94c6565a20d3b67b0ce49822606b07a96c96d786
Zaven Muradyan <zaven@voithos.io>
2024-08-16 17:10:16 -0700

    Fix pixel perfection.

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -9,7 +9,7 @@ var ENERGY_DROP_SCENE = preload("res://scenes/drops/energy_drop.tscn")
 func _ready() -> void:
 	var player = PLAYER_SCENE.instantiate()
 	add_child(player)
-	
+
 	var drop = ENERGY_DROP_SCENE.instantiate()
 	add_child(drop)
 
