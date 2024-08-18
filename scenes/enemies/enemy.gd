b3954cedc6d0295742e1ad67ea11a654da0d27d4
Zaven Muradyan <zaven@voithos.io>
2024-08-18 09:29:20 -0700

    Improve seeking

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -7,21 +7,25 @@ var health: HealthComponent = null
 var light_guns: Array[LightGun] = []
 var heavy_guns: Array[HeavyGun] = []
 
-var _direction_face := GSAIAgentLocation.new()
-
+## Steering behaviors
 var _steering: GSAISteeringBehavior
 
 @export var linear_drag_coefficient := 0.025
 @export var angular_drag := 0.1
 
 @onready var agent := await GSAICharacterBody2DAgent.new(self)
-@onready var player_agent: GSAISteeringAgent = Global.player.agent
 @onready var accel := GSAITargetAcceleration.new()
 
-@export var linear_accel_max := 3000.0
-@export var linear_speed_max := 120.0
+## Helper location that computes arrive distance from the player
+## Not active by default, you have to use it in `_setup_steering`
+var _player_seek_location := GSAIAgentLocation.new()
+
+@export var linear_accel_max := 1800.0
+@export var linear_speed_max := 150.0
 @export var angular_accel_max := 5000
 @export var angular_speed_max := 360
+
+@export var player_seek_offset := 50.0
 
 func _ready() -> void:
 	_init_components(self)
@@ -39,6 +43,11 @@ func _init_components(node: Node) -> void:
 		_init_components(child)
 
 
+func try_fire_light() -> void:
+	for gun in light_guns:
+		gun.try_fire()
+
+
 func _init_health_component(h: HealthComponent) -> void:
 	assert(health == null)
 	health = h
@@ -48,6 +57,16 @@ func _init_health_component(h: HealthComponent) -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+## Subclasses should override to prepare steering params / agents for a frame.
+func _steering_process(delta: float) -> void:
+	# Calculate offset by a fixed distance
+	var playerpos := Global.player.agent.position
+	var target := playerpos + (agent.position - playerpos).normalized() * player_seek_offset
+	_player_seek_location.position = target
+
+
+## Must assign to _steering
 func _setup_steering() -> void:
 	# Basic agent stuff.
 	agent.angular_acceleration_max = deg_to_rad(angular_accel_max)
@@ -55,8 +74,9 @@ func _setup_steering() -> void:
 	agent.linear_acceleration_max = linear_accel_max
 	agent.linear_speed_max = linear_speed_max
 
+
 func _physics_process(delta: float) -> void:
-	_direction_face.position = agent.position + accel.linear.normalized()
+	_steering_process(delta)
 
 	_steering.calculate_steering(accel)
 
@@ -64,7 +84,6 @@ func _physics_process(delta: float) -> void:
 		agent.angular_velocity + accel.angular * delta, -agent.angular_speed_max, agent.angular_speed_max
 	)
 	agent.angular_velocity = lerp(agent.angular_velocity, 0.0, angular_drag)
-
 	rotation += agent.angular_velocity * delta
 
 	var linear_velocity := GSAIUtils.to_vector2(agent.linear_velocity + accel.linear * delta)
--- scenes/enemies/gunner_enemy.gd
+++ scenes/enemies/gunner_enemy.gd
@@ -1,37 +1,37 @@
 extends Enemy
 
 
+
 func _ready() -> void:
 	super()
 	_setup_steering()
 
 # TODO: Try to fire the gun
 
+func _steering_process(delta: float) -> void:
+	super(delta)
+
+
 func _setup_steering() -> void:
 	super()
 
+	agent.bounding_radius = $CollisionShape2D.shape.radius
+
+	var player_agent := Global.player.agent
+
 	# Setup the core behavior.
-	var seek := GSAISeek.new(agent, player_agent)
-	var face := GSAIFace.new(agent, _direction_face)
+	var arrive := GSAIArrive.new(agent, _player_seek_location)
+	arrive.deceleration_radius = 10
+	var face := GSAIFace.new(agent, player_agent)
 	face.alignment_tolerance = deg_to_rad(5)
 	face.deceleration_radius = deg_to_rad(30)
 
-	agent.bounding_radius = $CollisionShape2D.shape.radius
-
-	var proximity := GSAIRadiusProximity.new(agent, [player_agent], 140)
-	var separation := GSAISeparation.new(agent, proximity)
-	separation.decay_coefficient = 200000
 	# This one does an "orbit" instead
 	#var avoid := GSAIAvoidCollisions.new(agent, proximity)
 
 	var blend := GSAIBlend.new(agent)
 
 	blend.add(face, 1)
-	blend.add(seek, 1)
-	blend.add(separation, 1)
-
-	var priority := GSAIPriority.new(agent, 0.0001)
-	priority.add(separation)
-	priority.add(blend)
+	blend.add(arrive, 1)
 
 	_steering = blend
