480bccc777aad0dc31ea1e39ff0fc7bacf1a5bb7
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2024-08-19 21:46:44 -0700

    Update level-up scale factor

--- scripts/config.gd
+++ scripts/config.gd
@@ -25,7 +25,7 @@ const PROJECTILES_GROUP := "projectiles"
 
 const DEFAULT_VIEWPORT_SIZE := Vector2(320, 180)
 
-const GROWTH_LEVEL_SCALE_FACTOR := 8.0
+const GROWTH_LEVEL_SCALE_FACTOR := 4.0
 
 const MAX_GROWTH_LEVEL := 3
 
